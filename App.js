f9ff52beb9c7e7cb4a16efdb2a297ccf45c81168
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-20 05:57:16 +0200

    update

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
