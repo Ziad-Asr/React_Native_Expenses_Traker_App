e71b3b26e0fbc2672d4892f53fc7e4de67910e4d
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-20 06:56:09 +0200

    update

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import ManageExpense from "./Screens/ManageExpense";
+import RecentExpenses from "./Screens/RecentExpenses";
+import AllExpenses from "./Screens/AllExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottomTap = createBottomTabNavigator();
+
+const ExpenseOverView = () => {
+  return (
+    <BottomTap.Navigator>
+      <BottomTap.Screen name="RecentExpenses" component={RecentExpenses} />
+      <BottomTap.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTap.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, World!</Text>
-    </View>
+    <>
+      <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="ExpenseOverView">
+          <Stack.Screen name="ManagaExpense" component={ManageExpense} />
+          <Stack.Screen
+            name="ExpenseOverView"
+            component={ExpenseOverView}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
